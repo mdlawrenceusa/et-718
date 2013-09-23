bc8275bf906f4453fe7ea9dc4bcef43bb1bff5d0
Michael Lawrence <mdlawrence@me.com>
2013-09-23 12:06:40 -0400

    Initial Commit
    Initial Commit

---
+++ project03a/project03.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+<meta content="en-us" http-equiv="Content-Language" />
+<meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+<title>Untitled 1</title>
+</head>
+
+<body>
+
+<h1>Binary Dice</h1>
+<h5>Demonstrates multiple if structure</h5>
+<h2>You rolled a 3</h2>
+<p><img alt="Die3" height="100" src="images/die3.jpg" width="100" />&nbsp;<br />
+In binary, that&#39;s 011</p>
+<p>Refresh this page to roll another die.</p>
+
+</body>
+
+</html>
